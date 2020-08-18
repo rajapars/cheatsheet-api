e6827ba5dafc0c7757164e59d93a6210dba25b78
Raja Parsaulian B <raja.parsaulian@gmail.com>
2020-08-19 02:18:57 +0700

    Initial Project using "spring intializr"

---
+++ src/test/java/xyz/tamanmain/cheatsheet/CheatsheetApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.tamanmain.cheatsheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheatsheetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
